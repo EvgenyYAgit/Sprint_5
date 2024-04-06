9d2b9f42194f691ad9523aff65c0015fe88967e2
Evgeny Krysenkov <eugene.krysenkov@yandex.com>
2024-04-06 10:22:10 +0300

    Исправлены локаторы, тесты проходят успешно

--- tests/description_of_elements.py
+++ tests/description_of_elements.py
@@ -1,35 +1,35 @@
 login_button = ".//button[text()='Войти в аккаунт']" #кнопка Войти в аккаунт
 header_input = "//h2[text()='Вход']" #заголовок Вход
 input_button = ".//button[text()='Войти']" #кнопка Войти
-input_button_registration = "//div[1]//p/a" #кнопка Войти в панели регистрации
-recovery_password_button = "//div[1]//p[2]/a" #кнопка Восстановить пароль
-exit_button = "//div[1]/div//nav/ul/li[3]/button" #кнопка Выход
+input_button_registration = "//a[text()='Войти']" #кнопка Войти в панели регистрации
+recovery_password_button = "//a[text()='Восстановить пароль']" #кнопка Восстановить пароль
+exit_button = "//button[text()='Выход']" #кнопка Выход
 
 register_button_string = ".//a[text()='Зарегистрироваться']" #строка-кнопка Зарегистрироваться
 register_button = ".//button[text()='Зарегистрироваться']" #кнопка Зарегистрироваться
 
-string_name = "//div[1]//fieldset[1]//input" #строка имя
-string_email = "//div[1]//fieldset[2]//input" #строка Email
-string_password = "//div[1]//fieldset[3]//input" #строка Пароль
+string_name = "//label[text()='Имя']/following::input[1]" #строка имя
+string_email = "//label[text()='Имя']/following::input[2]" #строка Email
+string_password = "//label[text()='Пароль']/following::input" #строка Пароль
 
-string_email_area_local = "//div[1]//fieldset[1]//input" #строка Еmail в личном кабинете
-string_password_area_local = "//div[1]//fieldset[2]//input" #строка Пароль в личном кабинете
+string_email_area_local = "//input[@type='text']" #строка Еmail в личном кабинете
+string_password_area_local = "//input[@type='password']" #строка Пароль в личном кабинете
 
 local_area_button = "//p[text()='Личный Кабинет']" #кнопка Личный Кабинет
 constructor_button = "//p[text()='Конструктор']" #кнопка Конструктор
-profile_button = "//div[1]//div/nav/ul/li[1]/a" #кнопка Профиль
+profile_button = "//a[text()='Профиль']" #кнопка Профиль
 
-error_line_incorrect_password = "//div[1]//fieldset[3]//p" #строка ошибки Некорректный пароль
+error_line_incorrect_password = "//p[text()='Некорректный пароль']" #строка ошибки Некорректный пароль
 
-button_place_an_order = "//div[1]//section[2]//button" #кнопка Оформить заказ
+button_place_an_order = "//button[text()='Оформить заказ']" #кнопка Оформить заказ
 
 logo = "[xmlns='http://www.w3.org/2000/svg'][width='290']" #логотип STELLAR BURGER
 
-sauces_section = "//div[1]//section[1]/div[1]/div[2]" #раздел Соусы
+sauces_section = "//span[text()='Соусы']/parent::div" #раздел Соусы
 tab_sauces_section = "[class='tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']" #раздел Соусы выбранный
 
-fillings_section = "//div[1]//section[1]/div[1]/div[3]" #раздел Начинки
+fillings_section = "//span[text()='Начинки']/parent::div" #раздел Начинки
 tab_fillings_section = "[class='tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']" #раздел Начинки выбранный
 
-buns_section = "//div[1]//section[1]/div[1]/div[1]" #раздел Булки
+buns_section = "//span[text()='Булки']/parent::div" #раздел Булки
 tab_buns_section = "[class='tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']" #раздел Булки выбранный
